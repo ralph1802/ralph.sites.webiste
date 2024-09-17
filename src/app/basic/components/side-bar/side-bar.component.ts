9337a2739f237d976372a6e6bc7b6002605e7919
Axel Leos <rleos1802@outlook.com>
2024-09-17 10:05:12 -0600

    creación del sidebar

--- src/app/basic/components/side-bar/side-bar.component.ts
+++ src/app/basic/components/side-bar/side-bar.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'side-bar',
   templateUrl: './side-bar.component.html',
-  styleUrl: './side-bar.component.css'
+  styleUrls: ['./side-bar.component.css']
 })
 export class SideBarComponent {
+  isSidebarOpen = false;
 
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.isSidebarOpen = false;
+      }
+    });
+  }
+
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
+
+  @HostListener('document:click', ['$event'])
+  closeSidebarOnClickOutside(event: MouseEvent) {
+    const sidebarElement = document.querySelector('.sidebar');
+    const buttonElement = document.querySelector('.sidebar-button button');
+    if (this.isSidebarOpen && sidebarElement && !sidebarElement.contains(event.target as Node) &&
+        buttonElement && !buttonElement.contains(event.target as Node)) {
+      this.isSidebarOpen = false;
+    }
+  }
 }
